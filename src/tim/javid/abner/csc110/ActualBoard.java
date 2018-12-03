08e506677d2103d995b4cff2a3a01c2aa0a0c09e
Abner <aturcios@student.neumont.edu>
2018-12-03 12:13:17 -0700

    Auto stash before merge of "master" and "origin/master"

--- src/tim/javid/abner/csc110/ActualBoard.java
+++ src/tim/javid/abner/csc110/ActualBoard.java
@@ -1,5 +1,12 @@
 package tim.javid.abner.csc110;
 
 public class ActualBoard {
+<<<<<<< Updated upstream
 	
 }
+=======
+
+	public BoardSpace[] spaces = new BoardSpace[40];
+	
+}
+>>>>>>> Stashed changes
