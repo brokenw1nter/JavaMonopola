4b2fbd512461387a126c71c4912729e4a8b71a87
Jofussaures <45467621+Jofussaures@users.noreply.github.com>
2018-12-03 12:47:37 -0700

    Added Arrays to ActualBoard

--- src/tim/javid/abner/csc110/ActualBoard.java
+++ src/tim/javid/abner/csc110/ActualBoard.java
@@ -4,4 +4,49 @@ public class ActualBoard {
 	
 	public BoardSpace[] spaces = new BoardSpace[40];
 	
+	public String[] placeLandOn(String[] spaces)	{
+		spaces[0] = "Go";
+		spaces[1] = "Brown";
+		spaces[2] = "Community Chest";
+		spaces[3] = "Brown";
+		spaces[4] = "Income Tax";
+		spaces[5] = "Reading Railroad";
+		spaces[6] = "Oriental Avenue";
+		spaces[7] = "Chance";
+		spaces[8] = "Vermont Avenue";
+		spaces[9] = "Connecticut Avenue";
+		spaces[10] = "Jail";
+		spaces[11] = "St. Charles Place";
+		spaces[12] = "Electric Company";
+		spaces[13] = "States Avenue";
+		spaces[14] = "Virginia Avenue";
+		spaces[15] = "Pennsylvania Railroad";
+		spaces[16] = "St. James Place";
+		spaces[17] = "Community Chest";
+		spaces[18] = "Tennessee Avenue";
+		spaces[19] = "New York Avenue";
+		spaces[20] = "Free Parkng";
+		spaces[21] = "Kentucky Avenue";
+		spaces[22] = "Chance";
+		spaces[23] = "Indiana Avenue";
+		spaces[24] = "Illinois Avenue";
+		spaces[25] = "B. & O. Railroad";
+		spaces[26] = "Atlntic Avenue";
+		spaces[27] = "Ventnor Avenue";
+		spaces[28] = "Water Works";
+		spaces[29] = "Marvin Gardens";
+		spaces[30] = "Go To Jail";
+		spaces[31] = "Pacific Avenue";
+		spaces[32] = "North Carolina Avenue";
+		spaces[33] = "Community Chest";
+		spaces[34] = "Pennsylvania Avenue";
+		spaces[35] = "Short Line";
+		spaces[36] = "Chance";
+		spaces[37] = "Park Place";
+		spaces[38] = "Luxury Tax";
+		spaces[39] = "Boardwalk";
+		
+		return spaces;
+		}
+	
 }
